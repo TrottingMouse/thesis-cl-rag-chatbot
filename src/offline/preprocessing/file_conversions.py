4210f5321ac7d3ec34afaa591f18efd872077062
Daniel Gelderblom <d.gelderblom1908@gmail.com>
2026-06-17 12:54:43 +0200

    split markdown processors

--- src/offline/preprocessing/file_conversions.py
+++ src/offline/preprocessing/file_conversions.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-# from docling.document_converter import DocumentConverter, PdfFormatOption
-# from docling.datamodel.pipeline_options import PdfPipelineOptions, TableFormerMode
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling.datamodel.pipeline_options import PdfPipelineOptions, TableFormerMode
 
 import pdfplumber
 
@@ -9,63 +9,91 @@ from src.models import Document
 from src.offline.preprocessing.base import BasePreprocessor
 import re
 import os
+from google import genai
 
-# class MarkdownProcessor(BasePreprocessor):
-#     """
-#     A preprocessor that uses Docling to convert raw documents (e.g. PDFs)
-#     into Markdown.
-#     """
+class GeminiMarkdownProcessor(BasePreprocessor):
+    """
+    A dummy preprocessor. Just loads the documents converted by Gemini 3.1 Pro.
+    """
+    def __init__(self):
+        super().__init__()
 
-#     def __init__(self):
-#         super().__init__()
-#         # Initialize the Docling converter
-#         pipeline_options = PdfPipelineOptions(do_table_structure=True)
+    @property
+    def name(self) -> str:
+        return "markdown_gemini"
 
-#         # 1. Force the high-accuracy model variant
-#         pipeline_options.table_structure_options.mode = TableFormerMode.ACCURATE
+    def preprocess(self, document: Document) -> Document:
+        new_id = document.doc_id + "_" + self.name
+        cached_path = "storage/cached_documents/" + new_id + ".txt"
+        if os.path.exists(cached_path):
+            with open(cached_path) as f:
+                content = f.read()
+            return Document(
+                source_path=document.source_path,
+                text=content,
+                preprocessor_name=self.name,
+                doc_id = new_id
+            )
+        else:
+            raise FileNotFoundError(f"Gemini markdown document not found at {cached_path}")
 
-#         # 2. Tell it to rely on its visual prediction rather than messy PDF text cells
-#         pipeline_options.table_structure_options.do_cell_matching = False 
+class DoclingMarkdownProcessor(BasePreprocessor):
+    """
+    A preprocessor that uses Docling to convert raw documents (e.g. PDFs)
+    into Markdown.
+    """
 
-#         # Pass options into your converter
-#         self.converter = DocumentConverter(
-#             format_options={
-#                 "pdf": PdfFormatOption(pipeline_options=pipeline_options)
-#             }
-#         )
+    def __init__(self):
+        super().__init__()
+        # Initialize the Docling converter
+        pipeline_options = PdfPipelineOptions(do_table_structure=True)
 
-#     @property
-#     def name(self) -> str:
-#         return "markdown"
+        # 1. Force the high-accuracy model variant
+        pipeline_options.table_structure_options.mode = TableFormerMode.ACCURATE
 
-#     def preprocess(self, document: Document) -> Document:
-#         source_path = document.source_path
-#         cached_path = "storage/cached_documents/" + document.doc_id + ".txt"
-#         if os.path.exists(cached_path):
-#             with open(cached_path) as f:
-#                 content = f.read()
-#             return Document(
-#                 source_path=document.source_path,
-#                 text=content,
-#                 preprocessor_name=self.name,
-#                 doc_id = document.doc_id + "_" + self.name
-#             )
+        # 2. Tell it to rely on its visual prediction rather than messy PDF text cells
+        pipeline_options.table_structure_options.do_cell_matching = False 
 
-#         # convert the document
-#         result = self.converter.convert(source_path)
-#         markdown_text = result.document.export_to_markdown()
+        # Pass options into your converter
+        self.converter = DocumentConverter(
+            format_options={
+                "pdf": PdfFormatOption(pipeline_options=pipeline_options)
+            }
+        )
 
-#         # save the document as txt
-#         with open(cached_path, "w") as outfile:
-#             outfile.write(markdown_text)
+    @property
+    def name(self) -> str:
+        return "markdown_docling"
 
-#         # Build and return the processed document
-#         return Document(
-#             source_path=document.source_path,
-#             text=markdown_text,
-#             preprocessor_name=self.name,
-#             doc_id=document.doc_id
-#         )
+    def preprocess(self, document: Document) -> Document:
+        source_path = document.source_path
+        new_id = document.doc_id + "_" + self.name
+        cached_path = "storage/cached_documents/" + new_id + ".txt"
+        if os.path.exists(cached_path):
+            with open(cached_path) as f:
+                content = f.read()
+            return Document(
+                source_path=document.source_path,
+                text=content,
+                preprocessor_name=self.name,
+                doc_id = document.doc_id + "_" + self.name
+            )
+
+        # convert the document
+        result = self.converter.convert(source_path)
+        markdown_text = result.document.export_to_markdown()
+
+        # save the document as txt
+        with open(cached_path, "w") as outfile:
+            outfile.write(markdown_text)
+
+        # Build and return the processed document
+        return Document(
+            source_path=document.source_path,
+            text=markdown_text,
+            preprocessor_name=self.name,
+            doc_id=document.doc_id
+        )
 
 
 class RawTextProcessor(BasePreprocessor):
@@ -117,11 +145,63 @@ class RawTextProcessor(BasePreprocessor):
             doc_id=new_id
         )
 
-#doc = Document(Path("documents/PO.pdf"), doc_id="PO")
+class LLMProcessor(BasePreprocessor):
+    def __init__(self, model="gemini-3.1-flash-lite"):
+        super().__init__()
+        self.model = model
+        
+
+    @property
+    def name(self) -> str:
+        return "llm_" + self.model
+    
+    def preprocess(self, document: Document) -> Document:
+        new_id = document.doc_id + "_" + self.name
+        cached_path = "storage/cached_documents/" + new_id + ".txt"
+        if os.path.exists(cached_path):
+            with open(cached_path) as f:
+                content = f.read()
+            return Document(
+                source_path=document.source_path,
+                text=content,
+                preprocessor_name=self.name,
+                doc_id = new_id
+            )
+
+        # convert the document
+        client = genai.Client()
+        response = client.models.generate_content(
+            model=self.model,
+            contents=f"""
+            
+            """
+        )
+        text = response.text
+        
+
+        # save the document as txt
+        with open(cached_path, "w") as outfile:
+            outfile.write(text)
+
+        # Build and return the processed document
+        return Document(
+            source_path=document.source_path,
+            text=text,
+            preprocessor_name=self.name,
+            doc_id=new_id
+        )
+
+doc = Document(Path("documents/PO.pdf"), doc_id="PO")
 
 # markdown_processor = MarkdownProcessor()
 # processed_document = markdown_processor.preprocess(doc)
 # print(processed_document)
-# raw_text_processor = RawTextProcessor()
+# gemini_markdown_processor = GeminiMarkdownProcessor()
+# # docling_markdown_processor = DoclingMarkdownProcessor()
+# # raw_text_processor = RawTextProcessor()
+# processed_document = gemini_markdown_processor.preprocess(doc)
+# print(processed_document)
+# processed_document = docling_markdown_processor.preprocess(doc)
+# print(processed_document)
 # processed_document = raw_text_processor.preprocess(doc)
 # print(processed_document)
