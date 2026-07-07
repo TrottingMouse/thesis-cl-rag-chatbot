d34f1198a13772c89b7a48e9996fc837f4e27ef1
Daniel Gelderblom <d.gelderblom1908@gmail.com>
2026-07-07 15:26:05 +0200

    refactor table chunker

--- src/offline/chunking/tables.py
+++ src/offline/chunking/tables.py
@@ -28,42 +28,95 @@ class WholeTableParagraphChunker(BaseChunker):
             A processed document whose `text` is ready for splitting.
         """
         chunks = []
-        paragraphs = re.split(r'\n\s*\n', document.text)
+        paragraphs = re.split(r'\n', document.text)
+        for paragraph in paragraphs:
+            print(paragraph)
+            print("--------------------------------")
         paragraphs = [p.strip() for p in paragraphs if p.strip()]
         previous_table = False
+        previous_module = False
+        current_chunk = ""
         
         for n_paragraph, paragraph in enumerate(paragraphs):
             if n_paragraph == 0:
                 continue
 
             if paragraph.startswith('|'):
-                chunks.append(Chunk(
-                    chunk_id=f"{document.doc_id}_chunk_{len(chunks)}", 
-                    text=paragraphs[n_paragraph-1] + "\n" + paragraph, 
-                    chunker_name=self.name
-                ))
-                previous_table = True
-                continue
-            if not previous_table:
-                chunks.append(Chunk(
-                    chunk_id=f"{document.doc_id}_chunk_{len(chunks)}", 
-                    text=paragraphs[n_paragraph-1], 
-                    chunker_name=self.name
-                ))
+                if not previous_table:
+                    chunks.append(Chunk(
+                        chunk_id=f"{document.doc_id}_chunk_{len(chunks)}", 
+                        text=current_chunk, 
+                        chunker_name=self.name
+                    ))
+                    previous_table = True
+                    current_chunk = paragraphs[n_paragraph-2] + "\n" + paragraphs[n_paragraph-1] + "\n"+ paragraphs[n_paragraph]
+
+                    chunks.pop(len(chunks) - 1)
+                    chunks.pop(len(chunks) - 1)
+                else:
+                    current_chunk += "\n" + paragraphs[n_paragraph]
+            elif paragraph.startswith("**"):
+                current_chunk += "\n" + paragraphs[n_paragraph]
+            else:
+                if previous_table:
+                    chunks.append(Chunk(
+                        chunk_id=f"{document.doc_id}_chunk_{len(chunks)}", 
+                        text="""**Abkürzungen im Studienverlaufsplan:**
+                            * Modulprüfung = MP
+                            * Studienleistung = SL
+                            * ECTS-Leistungspunkte = LP
+                            * Semesterwochenstunden = SWS
+                            * Profilbildungsbereich = PBB
+                            * Prüfungsnummer = Pnr.
+                            """
+                            + current_chunk, 
+                        chunker_name=self.name
+                    ))
+                    previous_table = False
+                    current_chunk = paragraphs[n_paragraph]
+                elif previous_module:
+                    chunks.append(Chunk(
+                        chunk_id=f"{document.doc_id}_chunk_{len(chunks)}", 
+                        text=current_chunk, 
+                        chunker_name=self.name
+                    ))
+                    previous_module = False
+                    current_chunk = paragraphs[n_paragraph]
+                    
+
+                else:
+                    chunks.append(Chunk(
+                        chunk_id=f"{document.doc_id}_chunk_{len(chunks)}", 
+                        text=current_chunk, 
+                        chunker_name=self.name
+                    ))
+                    current_chunk = paragraphs[n_paragraph]
             if n_paragraph == len(paragraphs) - 1:
+                if previous_table:
+                    text = """**Abkürzungen im Studienverlaufsplan:**
+* Modulprüfung = MP
+* Studienleistung = SL
+* ECTS-Leistungspunkte = LP
+* Semesterwochenstunden = SWS
+* Profilbildungsbereich = PBB
+* Prüfungsnummer = Pnr.
+""" + current_chunk
+                else:
+                    text = current_chunk
+
                 chunks.append(Chunk(
                     chunk_id=f"{document.doc_id}_chunk_{len(chunks)}", 
-                    text=paragraph, 
+                    text=text, 
                     chunker_name=self.name
                 ))
-            previous_table = False
+
             
             
         return chunks
 
 # write main function to test
 if __name__ == "__main__":
-    with open("storage/cached_documents/MH_markdown_gemini.txt") as f:
+    with open("storage/cached_documents/PO_markdown_gemini.txt") as f:
         text = f.read()
     document = Document(doc_id="test", text=text, source_path="")
     chunker = WholeTableParagraphChunker()
