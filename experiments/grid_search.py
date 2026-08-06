773198ee624f654d0a853de776fbefc98340a828
Daniel Gelderblom <d.gelderblom1908@gmail.com>
2026-08-06 19:53:17 +0200

    set params even higher for maxmin

--- experiments/grid_search.py
+++ experiments/grid_search.py
@@ -152,8 +152,8 @@ def _maxmin_configs() -> list[dict]:
                                          the default on this homogenous corpus)
     """
     configs = []
-    for ft, c in itertools.product((0.6, 0.7, 0.8), (1.1, 1.2, 1.3, 1.4)):
-        if ft + c > 1.7:
+    for ft, c in itertools.product((0.7, 0.8, 0.9), (1.2, 1.3, 1.4)):
+        if ft + c > 1.9:
             configs.append({"fixed_threshold": ft, "c": c})
     return configs
 
